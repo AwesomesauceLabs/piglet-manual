25630cd4893cc18a634b68e36562086fa9e5aebd
Ben Vandervalk <ben.vvalk@gmail.com>
2020-12-10 12:10:11 -0500

    add pandoc-filters/code-block-captions.lua [new][styling]
    Add and use a custom pandoc filter that move code block captions below
    their code blocks.
    
    By default, pandoc-crossref puts the captions above the code blocks.
    I thought this positioning was ugly and also inconsistent with the
    fact that figure captions appear their figures.

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ set -eu
 
 pandoc -s -o index.html -c css/classless.css \
        --highlight-style haddock --filter pandoc-crossref \
+       --lua-filter pandoc-filters/code-block-captions.lua \
        index.md
